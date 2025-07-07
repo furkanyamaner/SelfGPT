ce2932e744b69adc78b2b6f41adde2e6fefeff4a
furkanyamaner <127695978+furkanyamaner@users.noreply.github.com>
2025-07-08 01:38:48 +0300

    AutoTokenizer added and evaluated with new json file.

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 import torch as pt
 from torch import nn, optim
+from transformers import AutoProcessor
+text1 = "This is a sample text for tokenization."
+# Load the tokenizer
+processor = AutoProcessor.from_pretrained("google/gemma-3n-E4B-it")
 
-import tiktoken 
+# Tokenize text using the tokenizer
+processor.encode(text1)
